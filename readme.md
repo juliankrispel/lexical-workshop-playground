80c5a1994898aa3002d53337dbc49de33824fa02
Julian Krispel-Samsel <julian@jkrsp.com>
2022-07-06 13:56:00 +0100

    wip

--- readme.md
+++ readme.md
@@ -1,8 +1,14 @@
 # Getting started with lexical by jumping in the deep end
 
+## Preqrequisites
+
+- Basic knowledge of react
+- Some knowledge of typescript
+- Some knowledge of standard DOM apis for creating and updating html elements
+- Node version 12 or above - I've added an `nvmrc` file to make this easier.
+- [Yarn](https://yarnpkg.com/)
+
 ## Running the app
 
 1. Run `yarn install` to install all dependencies
 2. Run `yarn dev` to start your a dev server at localhost:3000
-
-## Setting up
--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,58 @@
 import "./App.css";
 
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin'
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-import TreeViewPlugin from "./plugins/TreeViewPlugin/TreeViewPlugin";
-import { ParagraphNode } from "./nodes/nodes";
-import { DraggablePlugin } from "./plugins/DraggablePlugin/DraggablePlugin";
-import { LexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import React from "react";
-import { createEditor } from "lexical";
+import { createEmptyHistoryState, HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 
 const initialState = {
   root: {
     children: [
       {
         type: "paragraph",
-        version: 0,
+        children: [
+          {
+            text: "Hello",
+            type: "text",
+          },
+          {
+            type: "linebreak",
+          },
+          {
+            text: "World",
+            type: "text",
+          },
+        ],
       },
     ],
-    direction: "ltr",
-    format: "left",
-    indent: 0,
     type: "root",
-    version: 1,
   },
 };
 
-const WithLexicalComposer = (props: React.PropsWithChildren<{}>) => {
+export default function App() {
+  const history = createEmptyHistoryState();
   return (
     <LexicalComposer
       initialConfig={{
         namespace: "test",
-        nodes: [ParagraphNode],
+        nodes: [],
         editorState: JSON.stringify(initialState),
         onError: (err) => {
           console.error(err);
         },
       }}
     >
-      <>{props.children}</>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div className="placeholder">Type something</div>}
+      />
+      <HistoryPlugin externalHistoryState={history} />
+      <OnChangePlugin
+        onChange={(editorState) => {
+          console.log(editorState.toJSON().root.children);
+        }}
+      />
     </LexicalComposer>
   );
-};
-
-export default function App() {
-  return (
-    <WithLexicalComposer>
-      <div className="editor-container">
-        <RichTextPlugin
-          contentEditable={<ContentEditable />}
-          placeholder={
-            <span className="placeholder">Please type something</span>
-          }
-        />
-        <DraggablePlugin />
-      </div>
-      <TreeViewPlugin />
-    </WithLexicalComposer>
-  );
-}
-
-function Placeholder() {
-  return <div className="editor-placeholder">Enter some plain text...</div>;
 }
